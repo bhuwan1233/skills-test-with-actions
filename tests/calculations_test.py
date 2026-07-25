d28c2121d4488adc94b5e4349353b596d1796b26
Bhuwan Bohara <98870773+bhuwan1233@users.noreply.github.com>
2026-07-25 13:30:50 +0545

    Revert "Reenable unit test that was disabled"

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -57,29 +57,14 @@ def test_get_nth_fibonacci_one():
     # Assert
     assert result == 1
 
-def test_get_nth_fibonacci_ten():
-    """Test with n=10."""
-    
-    # Arrange
-    n = 10
 
-    # Act
-    result = get_nth_fibonacci(n)
+# def test_get_nth_fibonacci_ten():
+#     """Test with n=10."""
+#     # Arrange
+#     n = 10
 
-    # Assert
-    assert result == 55
+#     # Act
+#     result = get_nth_fibonacci(n)
 
-def test_area_of_circle_negative_radius():
-    """Test with a negative radius."""
-    radius = -1
-
-    with pytest.raises(ValueError):
-        area_of_circle(radius)
-
-
-def test_get_nth_fibonacci_negative_number():
-    """Test with negative n."""
-    n = -1
-
-    with pytest.raises(ValueError):
-        get_nth_fibonacci(n)
+#     # Assert
+#     assert result == 89
